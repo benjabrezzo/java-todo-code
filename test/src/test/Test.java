5dfbc5d3c5f5c3e799f1e6d60a64e51c3201142b
benjabrezzo <benjabrezzo@gmail.com>
2024-01-25 13:17:00 -0300

    Solo para testear.

---
+++ test/src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ *
+ * @author benja
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Esto es un prueba para ver como concha funcina git y github en NetBeans.");
+    }
+    
+}
