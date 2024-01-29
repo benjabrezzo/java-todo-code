87fcd08b83e90b1b13d428553f3aab26e764f909
benjabrezzo <benjabrezzo@gmail.com>
2024-01-29 16:44:50 -0300

    Se crearon archivos para ver la cuestión de la interfaz gráfica y como se conecta con la lógica. Temas principales a tratar: Paquete Swing, JFrame y JPanel.

---
+++ PruebaCapas/src/logica/PruebaCapas.java
@@ -0,0 +1,20 @@
+package logica;
+
+import gui.Pantalla;
+
+
+/**
+ *
+ * @author benja
+ */
+public class PruebaCapas {
+    
+    public static void main(String[] args) {
+        
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true); //Mostramos la pantalla
+        panta.setLocationRelativeTo(null); //Ponemos la pantalla relativa a nada (null), ponela en el medio y listo.
+        
+    }
+    
+}
